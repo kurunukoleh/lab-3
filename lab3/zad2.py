3482273fbaa388a4cb928ef4bbe0bcedfb490c2f
kurunukoleh <85008916+kurunukoleh@users.noreply.github.com>
2025-11-05 15:05:10 +0100

    Add files via upload

--- lab3/zad2.py
+++ lab3/zad2.py
@@ -1,13 +1,14 @@
 licznik = 0
-a = False
+a = []
 
 
 while licznik <= 29:
-    licznik+=1
+    licznik += 1
     for i in range(2 , licznik):
         if licznik % i == 0:
-            a = True
+            a.append(False)
         else:
-            a = False
-    if a :
+            a.append(True)
+    if False not in a :
         print(licznik)
+    a=[]
